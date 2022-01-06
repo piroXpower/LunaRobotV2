82367c85a5f9f8327fa798943d3c8ba8e2aa4491
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-06 20:53:12 +0530

    Update gmute.py

--- lunaBot/modules/gmute.py
+++ lunaBot/modules/gmute.py
@@ -8,8 +8,9 @@ from telegram.ext import run_async, CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import mention_html
 
 import lunaBot.modules.sql.gmute_sql as sql
-from lunaBot import dispatcher, OWNER_ID, SUDO_USERS, STRICT_GMUTE
+from lunaBot import dispatcher, OWNER_ID, STRICT_GMUTE
 from lunaBot import DEMONS as SUPPORT_USERS
+from lunaBot import DEV_USERS as SUDO_USERS
 from lunaBot.modules.helper_funcs.chat_status import user_admin, is_user_admin
 from lunaBot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
 from lunaBot.modules.helper_funcs.filters import CustomFilters
